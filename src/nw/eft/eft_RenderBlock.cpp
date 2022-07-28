ecb272a60dbc810bc30335eb04c4806e8282a961
AboodXD <aboood40091@gmail.com>
2022-07-28 19:38:40 +0400

    Rename files

---
+++ src/nw/eft/eft_RenderBlock.cpp
@@ -0,0 +1,11 @@
+#include <nw/eft/eft_Renderer.h>
+#include <nw/eft/eft_Shader.h>
+
+namespace nw { namespace eft {
+
+void Renderer::BindParticleAttributeBlock(PtclAttributeBuffer* ptclAttributeBuffer, ParticleShader* shader, u32 numInstances)
+{
+    VertexBuffer::BindExtBuffer(5, sizeof(PtclAttributeBuffer) * numInstances, 0, sizeof(PtclAttributeBuffer), ptclAttributeBuffer);
+}
+
+} } // namespace nw::eft
